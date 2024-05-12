65d90f68ff6df6ea596d7db57c3970fa0042c244
leoworqu <132668626+leoworqu@users.noreply.github.com>
2024-05-12 10:26:10 +0300

    vs magic

---
+++ 0x02-minimum_operations/0-main.py
@@ -0,0 +1,7 @@
+minOperations = __import__('k').minOperations
+
+n = 9
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 17
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,12 @@
+def minOperations(n):
+    if n == 1:
+        return 0
+    
+    dp = [0] * (n+1)
+    for i in range(2, n+1):
+        dp[i] = float('inf')
+        for j in range(1, i):
+            if i % j == 0:
+                dp[i] = min(dp[i], dp[j] + i // j)
+                
+    return dp[n] if dp[n] != float('inf') else 0
