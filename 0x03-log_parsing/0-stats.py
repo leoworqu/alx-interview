1e5ce2ac9482d6599e07e18d151190081b3f8bbd
leoworqu <132668626+leoworqu@users.noreply.github.com>
2024-05-19 21:10:20 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
-"""Input stats"""
+"""Script to get stats from a request"""
 
 import sys
 
 codes = {}
-status_codes = [
-    '200',
-    '301',
-    '400',
-    '401',
-    '403',
-    '404',
-    '405',
-    '500'
-]
-sizes = 0
+status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
 count = 0
-
+size = 0
 
 try:
     for ln in sys.stdin:
