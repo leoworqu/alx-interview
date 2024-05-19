352900f482f6aa91ef011eccd63c1b57bfc4fb67
leoworqu <132668626+leoworqu@users.noreply.github.com>
2024-05-19 21:12:36 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,45 +1,49 @@
 #!/usr/bin/python3
-"""Script to get stats from a request"""
-
+"""
+    script that reads stdin line by line and computes metrics
+"""
 import sys
 
-codes = {}
-status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-count = 0
-size = 0
+
+def print_msg(codes, file_size):
+    print("File size: {}".format(file_size))
+    for key, val in sorted(codes.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
+
+
+file_size = 0
+code = 0
+count_lines = 0
+codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
 
 try:
-    for ln in sys.stdin:
-        if count == 10:
-            print("File size: {}".format(size))
-            for key in sorted(codes):
-                print("{}: {}".format(key, codes[key]))
-            count = 1
-        else:
-            count += 1
+    for line in sys.stdin:
+        parsed_line = line.split()
+        parsed_line = parsed_line[::-1]
 
-        ln = ln.split()
+        if len(parsed_line) > 2:
+            count_lines += 1
 
-        try:
-            size = size + int(ln[-1])
-        except (IndexError, ValueError):
-            pass
+            if count_lines <= 10:
+                file_size += int(parsed_line[0])
+                code = parsed_line[1]
 
-        try:
-            if ln[-2] in status_codes:
-                if codes.get(ln[-2], -1) == -1:
-                    codes[ln[-2]] = 1
-                else:
-                    codes[ln[-2]] += 1
-        except IndexError:
-            pass
+                if (code in codes.keys()):
+                    codes[code] += 1
 
-    print("File size: {}".format(size))
-    for key in sorted(codes):
-        print("{}: {}".format(key, codes[key]))
+            if (count_lines == 10):
+                print_msg(codes, file_size)
+                count_lines = 0
 
-except KeyboardInterrupt:
-    print("File size: {}".format(size))
-    for key in sorted(codes):
-        print("{}: {}".format(key, codes[key]))
-    raise
+finally:
+    print_msg(codes, file_size)
