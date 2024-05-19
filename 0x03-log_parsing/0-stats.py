39dcb870672452e4f737aeac9c12981b6afc169e
leoworqu <132668626+leoworqu@users.noreply.github.com>
2024-05-19 21:17:05 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,49 +1,30 @@
 #!/usr/bin/python3
-"""
-    script that reads stdin line by line and computes metrics
-"""
+""" Funct that count line by li"""
 import sys
 
 
-def print_msg(codes, file_size):
-    print("File size: {}".format(file_size))
-    for key, val in sorted(codes.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
-
-
-file_size = 0
-code = 0
-count_lines = 0
-codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
-
+i = 0
+FileSize = 0
+stats = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+code = ['200', '301', '400', '401', '403', '404', '405', '500']
 try:
     for line in sys.stdin:
-        parsed_line = line.split()
-        parsed_line = parsed_line[::-1]
-
-        if len(parsed_line) > 2:
-            count_lines += 1
-
-            if count_lines <= 10:
-                file_size += int(parsed_line[0])
-                code = parsed_line[1]
-
-                if (code in codes.keys()):
-                    codes[code] += 1
-
-            if (count_lines == 10):
-                print_msg(codes, file_size)
-                count_lines = 0
-
+        i += 1
+        sp = line.split(' ')
+        if len(sp) > 2:
+            FileSize += int(sp[-1])
+            if sp[-2] in stats:
+                stats[sp[-2]] += 1
+        if i % 10 == 0:
+            print("File size: {}".format(FileSize))
+            for f in code:
+                if stats[f]:
+                    print("{}: {}".format(f, stats[f]))
+except KeyboardInterrupt:
+    pass
 finally:
-    print_msg(codes, file_size)
+    print("File size: {}".format(FileSize))
+    for f in code:
+        if stats[f]:
+            print("{}: {}".format(f, stats[f]))
