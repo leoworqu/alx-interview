cb13d9db717ed4c95538de21f347cb8fc6250b6b
leoworqu <leoworqu@gmail.com>
2024-06-06 01:27:09 -0700

    fixed semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,32 +1,44 @@
 #!/usr/bin/node
 
-const axios = require('axios');
+const request = require('request');
 
-async function getCharacters(movieId) {
+function getCharacters (movieId) {
+  return new Promise((resolve, reject) => {
     const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-    const response = await axios.get(url);
-    const characters = response.data.characters;
-    return characters;
+    request.get(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        const characters = JSON.parse(body).characters;
+        resolve(characters);
+      }
+    });
+  });
 }
 
-async function printCharacters(movieId) {
-    try {
-        const characters = await getCharacters(movieId);
-        for (let i = 0; i < characters.length; i++) {
-            const characterResponse = await axios.get(characters[i]);
-            process.stdout.write(characterResponse.data.name);
-            if (i !== characters.length - 1) {
-                process.stdout.write('\n');
-            }
-        }
-    } catch (error) {
-        console.error("Error fetching characters:", error);
+function printCharacters (movieId, index) {
+  getCharacters(movieId).then(characters => {
+    if (index >= characters.length) {
+      return;
     }
+
+    request.get(characters[index], (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const characterName = JSON.parse(body).name;
+        console.log(characterName);
+        printCharacters(movieId, index + 1);
+      } else {
+        console.error('Error fetching character:', error);
+      }
+    });
+  }).catch(error => {
+    console.error('Error fetching characters:', error);
+  });
 }
 
 const movieId = process.argv[2];
 if (!movieId) {
-    console.error("Please provide a movie ID as an argument.");
+  console.error('Please provide a movie ID as an argument.');
 } else {
-    printCharacters(movieId);
+  printCharacters(movieId, 0);
 }
